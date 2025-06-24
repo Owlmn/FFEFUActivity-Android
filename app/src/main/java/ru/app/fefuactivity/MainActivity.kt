07124a27a6f2a1f8e16b54a94e2e7f3034695806
Owlmn <tregchannel@yandex.ru>
2025-06-24 16:33:11 +1000

    load unfinished lab2, checking git binding

---
+++ app/src/main/java/ru/app/fefuactivity/MainActivity.kt
@@ -0,0 +1,25 @@
+package ru.app.fefuactivity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_welcome)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        findViewById<com.google.android.material.button.MaterialButton>(R.id.AccountButton).setOnClickListener {
+            val intent = Intent(this, Registration::class.java)
+            startActivity(intent)
+        }
+    }
+}
